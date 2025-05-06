8ad08e7401eb02a5792c1748466a319d9eaace61
alaa-barazi <brazialaa@gmail.com>
2025-05-06 18:30:25 +0300

    updated connection

--- sampleClient/src/MainPageController.java
+++ sampleClient/src/MainPageController.java
@@ -4,54 +4,42 @@ import javafx.scene.control.TextArea;
 
 public class MainPageController {
 
-    @FXML
-    private TextArea infoArea;
+	@FXML
+	private TextArea infoArea;
 
-    @FXML
-    public void initialize() {
-        // Override display so messages from server go into infoArea
-        Main.clientConsole = new ClientConsole(Main.serverIP, 5555) {
-            @Override
-            public void display(Object message) {
-                Platform.runLater(() -> {
-                    if (message instanceof String str) {
-                        infoArea.setText(str);
-                    }
-                });
-            }
-        };
-    }
-
-    public void handleConnectionInfo() {
-        Main.clientConsole.accept("clientDetails");
-    }
-
-    public void handleOrders() {
-        try {
-            Main.switchScene("ShowOrder.fxml");
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
-
-    public void handleUpdate() {
-        try {
-            Main.switchScene("UpdateOrder.fxml");
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
-    
-    @FXML
-	public void handleExit() {
-		try {
-			if (Main.clientConsole != null && Main.clientConsole.client != null) {
-				Main.clientConsole.client.closeConnection();
-				System.out.println("Client closed connection via Exit button");
+	@FXML
+	public void initialize() {
+		// Override display so messages from server go into infoArea
+		Main.clientConsole = new ClientConsole(Main.serverIP, 5555) {
+			@Override
+			public void display(Object message) {
+				Platform.runLater(() -> {
+					if (message instanceof String str) {
+						infoArea.setText(str);
+					}
+				});
 			}
+		};
+	}
+
+	public void handleConnectionInfo() {
+		Main.clientConsole.accept("clientDetails");
+	}
+
+	public void handleOrders() {
+		try {
+			Main.switchScene("ShowOrder.fxml");
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
-		Platform.exit(); // Close the JavaFX window
 	}
+
+	public void handleUpdate() {
+		try {
+			Main.switchScene("UpdateOrder.fxml");
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
 }
